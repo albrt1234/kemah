73cdcaa73a08232b0f1db19a39f2c878d8dcd2b9
albrt1234 <lolalbert777@gmail.com>
2025-09-19 22:21:06 +0700

    Update addFileNamesNew.js

--- public/javascripts/addFileNamesNew.js
+++ public/javascripts/addFileNamesNew.js
@@ -1,10 +1,17 @@
-function previewMultiple(event) {
-    const form = document.querySelector('#formFile');
-    form.innerHTML = "";
-    const images = document.getElementById("image");
-    const number = images.files.length;
-    for (i = 0; i < number; i++) {
-        const urls = URL.createObjectURL(event.target.files[i]);
-        form.innerHTML += '<img src="' + urls + '">';
-    }
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const imageInput = document.getElementById("image");
+    const previewDiv = document.getElementById("formFile");
+
+    imageInput.addEventListener("change", (event) => {
+        previewDiv.innerHTML = "";
+        const files = event.target.files;
+        for (let i = 0; i < files.length; i++) {
+            const url = URL.createObjectURL(files[i]);
+            const img = document.createElement('img');
+            img.src = url;
+            img.style.width = '100px';
+            img.style.margin = '5px';
+            previewDiv.appendChild(img);
+        }
+    });
+});
